ec5076b8cf99f20841652297076b7a33bf381fc4
Jorge Valdez <jorgedanielvaldezcamacho@gmail.com>
2018-04-23 11:36:03 -0600

    Initial commit

---
+++ Examen2daUnidad/Operaciones.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2daUnidad
+{
+    class Operaciones
+    {
+        public int suma(int[] nums)
+        {
+            int sumanums = 0;
+            for(int x=0;x<nums.Length;x++)
+            {
+                sumanums = sumanums + nums[x];
+            }
+            return sumanums;
+        }
+
+        public double promedio(int[] nums)
+        {
+            double sumanums = 0;
+            for (int x = 0; x < nums.Length; x++)
+            {
+                sumanums += nums[x];
+            }
+            return sumanums / 10;
+        }
+
+        public int moda(int[] nums)
+        {
+            int frecTemp, frecModa = 0, moda1 = -1;
+            for (int x = 0; x < nums.Length; x++)
+            {
+                frecTemp = 1;
+                for (int y = x + 1; y < nums.Length; y++)
+                {
+                    if (nums[x] == nums[y])
+                        frecTemp++;
+                }
+                if (frecTemp > frecModa)
+                {
+                    frecModa = frecTemp;
+                    moda1 = nums[x];
+                }
+            }
+            return moda1;
+        }
+
+        public int mediana(int[] nums)
+        {
+            int aux = 0;
+            int pos = 0;
+            int z = 0;
+            int x = 0;
+            for (z = 0; z < nums.Length; z++)
+            {
+                pos = z;
+                for (x = z + 1; x < nums.Length; x++)
+                {
+                    if (nums[x] < nums[pos])
+                    {
+                        pos = x;
+
+                    }
+                }
+                aux = nums[z];
+                nums[z] = nums[pos];
+                nums[pos] = aux;
+
+            }
+            return ((nums[4] + nums[5]) / 2);
+        }
+    }
+}
---
+++ Examen2daUnidad/TestClass.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2daUnidad
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestSuma()
+        {
+            int[] arreglo = new int[] { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            Operaciones nuevo = new Operaciones();
+            var esperado = 84;
+            Assert.AreEqual(nuevo.suma(arreglo), esperado);
+        }
+
+        [Test]
+        public void TestPromedio()
+        {
+            int[] arreglo = new int[] { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            Operaciones nuevo = new Operaciones();
+            double esperado = 8.4;
+            double obtenido = nuevo.promedio(arreglo);
+            Assert.AreEqual(obtenido, esperado);
+        }
+
+        [Test]
+        public void TestModa()
+        {
+            int[] arreglo = new int[] { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            Operaciones nuevo = new Operaciones();
+            var esperado = 9;
+            Assert.AreEqual(nuevo.moda(arreglo), esperado);
+        }
+
+        [Test]
+        public void TestMediana()
+        {
+            int[] arreglo = new int[] { 5, 7, 8, 8, 9, 9, 9, 9, 10, 10 };
+            Operaciones nuevo = new Operaciones();
+            var esperado = 9;
+            Assert.AreEqual(nuevo.mediana(arreglo),esperado);
+        }
+    }
+}
